4de23e9d3e6bb27942339cb8e924bf173e97dc15
zhaixiaofan <zhaixiaofan@qq.com>
2017-11-14 10:22:22 +0800

    init

---
+++ app/src/main/java/com/example/zhai/roundimageview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.zhai.roundimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        RoundImageView imageView = (RoundImageView) findViewById(R.id.iv_picture);
+//        RoundImageView imageView2 = (RoundImageView) findViewById(R.id.iv_picture2);
+//        imageView.setType(0);
+//        imageView2.setType(1);
+
+    }
+}
